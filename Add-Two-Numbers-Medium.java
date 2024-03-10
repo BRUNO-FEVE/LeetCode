1cd1e0820b83b02b0629445c16c900283624cd20
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-03-10 17:54:42 -0300

    add: Add-Two-Numbers - Medium

---
+++ Add-Two-Numbers-Medium.java
@@ -0,0 +1,43 @@
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {  
+
+      ListNode curr1 = l1;
+      ListNode curr2 = l2;
+      ListNode head = null;
+      ListNode temp = null;
+
+      int rise = 0;
+
+      while (curr1 != null || curr2 != null || rise != 0) {
+
+        if(curr1 == null) curr1 = new ListNode(0) ;
+        if(curr2 == null) curr2 = new ListNode(0);
+
+        int number = curr1.val + curr2.val + rise;
+        
+        if(rise == 1) {
+           rise -= 1; 
+        }
+
+        if (number >= 10) {
+          number -= 10;
+          rise += 1;
+        }
+
+        ListNode current = new ListNode(number);
+
+        if(temp == null) {
+            temp = new ListNode(number);
+            head = temp;
+        } else {
+            temp.next = current;
+            temp = current;
+        }
+
+        curr1 = curr1.next;
+        curr2 = curr2.next;
+      }
+
+       return  head;
+  }
+}
