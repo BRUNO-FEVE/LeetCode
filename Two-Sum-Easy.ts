14aeaa3c2d0bb278ba8ebec74fcc126fd4358a9f
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-03-10 18:02:59 -0300

    add: Two Sum - Easy

---
+++ Two-Sum-Easy.ts
@@ -0,0 +1,19 @@
+// Problem Link: https://leetcode.com/problems/two-sum/
+
+// Time and Space Complexity: O(nˆ2) 
+
+// Performace:
+//     RunTime:   130 ms      ( better then 18,49% of users of TypeScript)
+//      Memory:   51.76 MB  ( better then 79,20% of users of TypeScript) 
+
+function twoSum(nums: number[], target: number): number[] {
+  for(let i=0; i<nums.length; i++) {
+      let amostTarget = target - nums[i];
+
+      nums[i] = undefined
+
+      if(nums.includes(amostTarget)) {
+          return [ i, nums.indexOf(amostTarget) ]
+      }
+  }
+};
