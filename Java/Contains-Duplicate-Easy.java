dcb8beb2cd0eaee934e9ff116dfdf94ea19de24f
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-03-25 17:00:23 -0300

    add: Contains Duplicate - Easy

--- Java/Contains-Duplicate-Easy.java
+++ Java/Contains-Duplicate-Easy.java
@@ -1,9 +1,31 @@
+// Problem Link: https://leetcode.com/problems/contains-duplicate/description/
+
+// First Solution 
+
+// Space Complexity: O(n)
+//  Time Complexity: O(n)
+
+// Performace:
+//     RunTime:   11 ms      ( better then 75,22% of users of Java )
+//      Memory:   58.89 MB   ( better then 56,89% of users of Java ) 
+
 package Java;
 
+import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
   public boolean containsDuplicate(int[] nums) {
-      Set<Integer> nasBeen = new Set<Integer>() ;
+      Set<Integer> hasBeen = new HashSet<Integer>() ;
+
+      for(int i=0; i<nums.length; i++) {
+        if (hasBeen.contains(nums[i])) {
+          return true;
+        } else {
+          hasBeen.add(nums[i]);
+        }
+      }
+
+      return false;
   }
 }
